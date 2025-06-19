22d796f8511da55fae6e3476338bd3e12c66eeaa
--global <st10477570@rcconnect.edu.za>
2025-06-19 11:29:56 +0000

    added lightbox updated digital pharmacy

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ document.getElementById("more").onclick = function () {
     document.getElementById("showmo").classList.add("visible");
   }, 600); // Adjusted for scroll timing
 };
+function preview(src) {
+  document.getElementById("previewpict").src = src;
+  document.getElementById("preview").style.display = "flex"; /*Lightbox function*/
+}
+
+function closepreview() {
+  document.getElementById("preview").style.display = "none"; /*closes the lightbox*/
+}
